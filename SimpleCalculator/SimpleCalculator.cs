6d39e3f2e9db24e70e4bd28ad2d37c313e2588d5
ICreateThunder <iamrobshalr@gmail.com>
2023-10-23 10:54:02 +0100

    [+] Removed unnessecary includes, implemented simple calculator

---
+++ SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,27 @@
+﻿using CalculatorApi;
+
+namespace CalculatorImpl
+{
+    public class SimpleCalculator : ISimpleCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return (a * b);
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
